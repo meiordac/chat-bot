6e0435804ecaec49fc618afca2470c5a7b1a0333
Matias Iordache <matias.iordache@gmail.com>
2019-06-24 20:37:56 +0200

    add hello world

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ def get_response():
     return jsonify(response)
 
 
+@app.route('/', methods=['GET'])
+def index():
+    return 'Hello World!'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
